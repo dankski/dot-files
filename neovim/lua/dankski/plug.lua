e4046b39471db670eedd56d594565f482cf8041f
Giacomo Amoroso <giacomo@amoroso.me>
2025-01-05 16:25:38 +0100

    NeoVim: Update plugin dependecies

--- neovim/lua/dankski/plug.lua
+++ neovim/lua/dankski/plug.lua
@@ -1,61 +1,61 @@
+-- Automatically run: PackerCompile
+vim.api.nvim_create_autocmd("BufWritePost", {
+  group = vim.api.nvim_create_augroup("PACKER", { clear = true }),
+  pattern = "plugins.lua",
+  command = "source <afile> | PackerCompile",
+})
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use) 
 
-  -- Packer can manage itself
-  use( 'wbthomason/packer.nvim' )
-  use( 'folke/tokyonight.nvim' )
+  -- Packer
+  use("wbthomason/packer.nvim")
 
-  -- Fuzzy Search
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = {'nvim-lua/plenary.nvim'}
-  }
+  -- Mason: For external tool management
+  use("williamboman/mason.nvim")
+  use("williamboman/mason-lspconfig.nvim")
 
-  -- Brackets Autopair
-  use { 'windwp/nvim-autopairs' }
-
-  -- startup screen
-  use ( 'mhinz/vim-startify' )
-
-  -- statusline
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons' , opt = true } 
-  }
-
-  -- treesitter
-  use ('nvim-treesitter/nvim-treesitter', {
-    run = ":TSUpdate"
+  -- Telescope: Fuzzy Search
+  use({
+      "nvim-telescope/telescope.nvim",
+      tag = "0.1.5",
+      branch = "0.1.x",
+      requires = { { "nvim-lua/plenary.nvim" } },
   })
 
-  use( 'nvim-treesitter/playground' )
+  -- statusline
+  use ({
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons' , opt = true } 
+  })
 
-  -- LSP zero: https://github.com/VonHeikemen/lsp-zero.nvim
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    requires = {
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
-  
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-buffer'},
-      {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/cmp-nvim-lua'},
-  
-      -- Snippets
-      {'L3MON4D3/LuaSnip'},
-      {'rafamadriz/friendly-snippets'},
-    }
-  }
+  -- startup screen
+  use ("mhinz/vim-startify")
 
-  -- use('romgrk/nvim-treesitter-context' )
-  use( 'tpope/vim-fugitive' )
+  -- Coding Setup: Rust
+  use("neovim/nvim-lspconfig")
+  use("simrat39/rust-tools.nvim")
 
+  -- Completion framework:
+  use 'hrsh7th/nvim-cmp' 
+
+  -- LSP completion source:
+  use 'hrsh7th/cmp-nvim-lsp'
+
+  -- Useful completion sources:
+  use 'hrsh7th/cmp-nvim-lua'
+  use 'hrsh7th/cmp-nvim-lsp-signature-help'
+  use 'hrsh7th/cmp-vsnip'                             
+  use 'hrsh7th/cmp-path'                              
+  use 'hrsh7th/cmp-buffer'                            
+  use 'hrsh7th/vim-vsnip'
+
+  -- Nord Theme
+  use("shaunsingh/nord.nvim")
+
+  -- Gruvbox Theme
+  use { "ellisonleao/gruvbox.nvim" }
+
+  -- DAP
+  use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} }
+  use { "leoluz/nvim-dap-go" }
 end)
-
-
